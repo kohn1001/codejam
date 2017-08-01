b8b00860badfda9519d4133a9540a767e197310f
K1001 <kohn1001@gmail.com>
2017-08-01 20:58:37 +0300

    add the case of negative

--- string_to_int.cpp
+++ string_to_int.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 
 /*    str = "1001"
@@ -9,16 +10,30 @@
 
 using namespace std;
 
-int string_to_int(string str)
+int string_to_int(string &&str)
 {
 	int res = 0;
-	for(int i =0; i < str.size(); ++i) {
+	int sign = 1, i;
+	if (str.size() == 0) { return 0; }
+	if (str[0] == '-') {
+		sign = -1;
+		i = 1;
+	}
+	else {
+		i = 0;
+	}
+	for(; i < str.size(); ++i) {
 		res = 10*res + (str[i] - '0');
 	}
+	return sign * res;
 }
 
 int main()
 {
-	cout << " string to int\n";
+	string str;
+	cout << "Enter the string you want to covert to int: " << endl;
+	cin >> str;
+	int i = string_to_int(move(str));
+	cout << "conversion of " << str << " to int is: " << i << endl;
 	return 0;
 }
